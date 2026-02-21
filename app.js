fb25c01a136b5558b14a115889db49583312de1e
Muhammad Shafique <msnonari73@gmail.com>
2026-02-21 15:23:44 +0500

    Static middleware setup with path module

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 import express from "express";
+import path from "node:path";
+import dotenv from "dotenv";
+import { fileURLToPath } from "node:url";
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
