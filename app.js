95be6f4faae0199044de57b7c59800b8749299a8
Muhammad Shafique <msnonari73@gmail.com>
2026-02-24 15:09:46 +0500

    Create user and get all user

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from "node:path";
 import dotenv from "dotenv";
 import { fileURLToPath } from "node:url";
 import { connectDB } from "./config/db.js";
+import { User } from "./models/User.js";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -18,6 +19,28 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/adduser", async (req, res) => {
+  try {
+    await User.create({
+      firstName: "Qasim",
+      lastName: "Zia",
+      email: "zia@gmail.com",
+      password: "5544sdfsd",
+      mobile: "0300-4455656",
+      address: "Kamalia",
+    });
+    console.log("Record added.");
+    res.send("Record added.");
+  } catch (err) {
+    console.log(err.message);
+    res.send(err.message);
+  }
+});
+app.get("/getall", async (req, res) => {
+  const data = await User.find();
+  res.json(data);
 });
 
 app.listen(port, () => {
